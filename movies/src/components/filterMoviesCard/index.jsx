e93affe7997ae44ef32adaa6e66a102163713219
dw <dwfixe@gmail.com>
2025-10-27 21:56:02 +0000

    Added filtering of movie list - Movie lab 1 complete!

--- movies/src/components/filterMoviesCard/index.jsx
+++ movies/src/components/filterMoviesCard/index.jsx
@@ -19,86 +19,86 @@ const formControl =
 
 export default function FilterMoviesCard(props) {
 
-  const [genres, setGenres] = useState([{ id: '0', name: "All" }])
+    const [genres, setGenres] = useState([{ id: '0', name: "All" }])
 
-    useEffect(() => {
-    fetch(
-        "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-        import.meta.env.VITE_TMDB_KEY
-    )
-        .then(res => res.json())
-        .then(json => {
-            console.log(json.genres) 
-            return json.genres
-        })
-        .then(apiGenres => {
-            setGenres([genres[0], ...apiGenres]);
-        });
-        // eslint-disable-next-line
-  }, []);
+        useEffect(() => {
+        fetch(
+            "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+            import.meta.env.VITE_TMDB_KEY
+        )
+            .then(res => res.json())
+            .then(json => {
+                // onsole.log(json.genres)
+                return json.genres
+            })
+            .then(apiGenres => {
+                setGenres([genres[0], ...apiGenres]);
+            });
+            // eslint-disable-next-line
+    }, []);
 
-  const handleChange = (e, type, value) => {
-    e.preventDefault()
-    // Completed later
-  };
-  const handleTextChange = e => {
-    handleChange(e, "name", e.target.value)
-  }
-  const handleGenreChange = e => {
-    handleChange(e, "genre", e.target.value)
-  };
+    const handleChange = (e, type, value) => {
+        e.preventDefault()
+        props.onUserInput(type, value)   // NEW
+    }
+    const handleTextChange = e => {
+        handleChange(e, "name", e.target.value)
+    }
+    const handleGenreChange = e => {
+        handleChange(e, "genre", e.target.value)
+    };
 
-  return (
-    <Card 
-    sx={{
-        backgroundColor: "rgb(204, 204, 0)"
-    }}
-    variant="outlined">
-    <CardContent>
-        <Typography variant="h5" component="h1">
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-        </Typography>
-        <TextField
-            sx={{...formControl}}
-            id="filled-search"
-            label="Search field"
-            type="search"
-            variant="filled"
-            value={props.titleFilter}
-            onChange={handleTextChange}
+    return (
+        <Card 
+        sx={{
+            backgroundColor: "rgb(204, 204, 0)"
+        }}
+        variant="outlined">
+        <CardContent>
+            <Typography variant="h5" component="h1">
+            <SearchIcon fontSize="large" />
+            Filter the movies.
+            </Typography>
+            <TextField
+                sx={{...formControl}}
+                id="filled-search"
+                label="Search field"
+                type="search"
+                variant="filled"
+                value={props.titleFilter}
+                onChange={handleTextChange}
+            />
+            <FormControl sx={{...formControl}}>
+                <InputLabel id="genre-label">Genre</InputLabel>
+                <Select
+                    labelId="genre-label"
+                    id="genre-select"
+                    defaultValue=""
+                    value={props.genreFilter}
+                    onChange={handleGenreChange}
+                >
+                {genres.map((genre) => {
+                    return (
+                        <MenuItem key={genre.id} value={genre.id}>
+                            {genre.name}
+                        </MenuItem>
+                );
+                })}
+            </Select>
+            </FormControl>
+        </CardContent>
+        <CardMedia
+            sx={{ height: 300 }}
+            image={img}
+            title="Filter"
         />
-        <FormControl sx={{...formControl}}>
-            <InputLabel id="genre-label">Genre</InputLabel>
-            <Select
-                labelId="genre-label"
-                id="genre-select"
-                defaultValue=""
-                value={props.genreFilter}
-                onChange={handleGenreChange}
-            >
-            {genres.map((genre) => {
-                return (
-                    <MenuItem key={genre.id} value={genre.id}>
-                        {genre.name}
-                    </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-    </CardContent>
-    <CardMedia
-        sx={{ height: 300 }}
-        image={img}
-        title="Filter"
-    />
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-          <br />
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+        <CardContent>
+            <Typography variant="h5" component="h1">
+            <SearchIcon fontSize="large" />
+            Filter the movies.
+            <br />
+            </Typography>
+        </CardContent>
+        </Card>
+    );
 }
